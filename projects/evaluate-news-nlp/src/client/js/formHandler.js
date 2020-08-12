cc47bc1ff248b24a841404014690a4f0b90b64fa
Hilde Pols <atschpe@gmail.com>
2020-08-12 15:16:59 +0200

    Initial Commit
    figuring out why /getApi endpoint is not working

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,51 @@
+/* Global Variables */
+const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
+const apiHolder = '?key=';
+const queryHolder = '&of=json&txt='
+const langHolder= '&lang=en';
+
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
+    // get input from form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+	//get api
+	getKey('/getApi')
+	.then( async function(data) { 
+		const request = await fetch(baseUrl + apiHolder + data.key + queryHolder + formText + langHolder)
+		try {
+			const receivedData = await request.json();
+			console.log(receivedData);
+			return receivedData;
+		} catch (error) {
+			console.log('error', error);
+		}
+	}) 
+	.then(function (res) {
+		document.getElementById('results').innerHTML = res.message
+	})
+	
+//    console.log("::: Form Submitted :::")
+//    fetch('http://localhost:8080/test')
+//    .then(res => res.json())
+//    .then(function(res) {
+//        document.getElementById('results').innerHTML = res.message
+//    })
+}
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+async function getKey(server) {
+	const request = await fetch(server, {
+		method: 'GET',
+		credentials: 'same-origin',
+		//headers: 'Content-Type': 'application/json'
+	});
+	try {
+		const data = await response.json();
+		console.log("Api key has been retrieved");
+		return data;
+	} catch (error) {
+		console.log('error', error);
+	}
 }
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,10 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const meaningCloud = require('meaning-cloud')
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+/*adding*/
+const bodyPaser = require('body-parser')
+const cors = require('cors')
+
 const app = express()
+/*adding*/
+app.use(cors())
+app.use(bodyPaser.json()) //for json
+app.use(bodyPaser.urlencoded()) { // for url-encoded values
+	extended: true
+}
 
 app.use(express.static('dist'))
+
+// Safely stored the API away from prying eyes and make available through getter
+app.get('/getApi', function(req, res) {
+	res.send(process.env.API_KEY);
+})
 
 console.log(__dirname)
 
